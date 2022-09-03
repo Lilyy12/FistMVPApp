292bf432d1efd642f8a2a1d16dac8a4d1ff4fe48
lilyy12 <lily.mingt@gmail.com>
2022-09-03 21:35:32 +0100

    more infos

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FirstMVCApp.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FirstMVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        private static List<DogViewModel> dogs = new List<DogViewModel>();
+
+        public IActionResult Index()
+        {
+            return View(dogs);
+        }
+    
+
+        public IActionResult Create()
+        {
+            var dogVm = new DogViewModel();
+            return View(dogVm);
+        }
+
+        public IActionResult CreateDog(DogViewModel dogViewModel)
+        {
+            // return View("Index");
+            dogs.Add(dogViewModel);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public string Hello()
+        {
+            return "Who's there?";
+        }
+    }
+}
+
---
+++ Models/DogViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace FirstMVCApp.Models
+{
+    public class DogViewModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
+
